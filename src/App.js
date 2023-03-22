dd902e4ec5c58a4ff202dfe71baa84b5202a1758
Thoba Shangase <shangasett@outlook.com>
2023-03-22 07:32:50 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,159 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div id="top"></div>
+      {/* <!-- Navigation--> */}
+        <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div className="container">
+                <a className="navbar-brand" href="#top">Thoba Shangase</a>
+                <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i className="fas fa-bars"></i>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Portfolio</a></li>
+                        <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="#services">Services</a></li>
+                        <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        {/* <!-- Masthead--> */}
+        <header className="masthead bg-primary text-white text-center">
+            <div className="container d-flex align-items-center flex-column">
+                {/* <!-- Masthead Avatar Image--> */}
+                <img className="masthead-avatar mb-5 rounded-circle" src="avatar.jpg" alt="..." />
+                {/* <!-- Masthead Heading--> */}
+                <h1 className="masthead-heading text-uppercase mb-0">Thoba Shangase</h1>
+                {/* <!-- Icon Divider--> */}
+                <div className="divider-custom divider-light">
+                    <div className="divider-custom-line"></div>
+                    <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+                    <div className="divider-custom-line"></div>
+                </div>
+                {/* <!-- Masthead Subheading--> */}
+                <p className="masthead-subheading font-weight-light mb-0">Software Developer</p>
+            </div>
+        </header>
+        {/* <!-- Portfolio Section--> */}
+        <section className="page-section portfolio" id="portfolio">
+            <div className="container">
+                {/* <!-- Portfolio Section Heading--> */}
+                <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Portfolio</h2>
+                {/* <!-- Icon Divider--> */}
+                <div className="divider-custom">
+                    <div className="divider-custom-line"></div>
+                    <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+                    <div className="divider-custom-line"></div>
+                </div>
+                {/* <!-- Portfolio Grid Items--> */}
+                <div className="row justify-content-center">
+                    {/* <!-- Portfolio Item 1--> */}
+                    <div className="col-md-6 col-lg-4 mb-5">
+                        <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                            </div>
+                            <img className="img-fluid" src="assets/img/portfolio/cabin.png" alt="..." />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {/* <!-- Services Section--> */}
+        <section className="page-section bg-primary text-white mb-0" id="services">
+            <div className="container">
+                {/* <!-- Services Section Heading--> */}
+                <h2 className="page-section-heading text-center text-uppercase text-white">Services</h2>
+                {/* <!-- Icon Divider--> */}
+                <div className="divider-custom divider-light">
+                    <div className="divider-custom-line"></div>
+                    <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+                    <div className="divider-custom-line"></div>
+                </div>
+                {/* <!-- Services Section Content--> */}
+                <div className="row">
+                    <div className="col-lg-4 ms-auto">
+                        <h3>Test Heading</h3>
+                        <p className="lead">
+                            Freelancer is a free bootstrap theme created by Thoba Shangase. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.
+                        </p>
+                    </div>
+                    <div className="col-lg-4 ms-auto">
+                        <h3>Test Heading</h3>
+                        <p className="lead">
+                            Freelancer is a free bootstrap theme created by Thoba Shangase. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.
+                        </p>
+                    </div>
+                    <div className="col-lg-4 me-auto">
+                        <h3>Test Heading</h3>
+                        <p className="lead">
+                              You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {/* <!-- Contact Section--> */}
+        <section className="page-section" id="contact">
+            <div className="container">
+                {/* <!-- Contact Section Heading--> */}
+                <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Me</h2>
+                {/* <!-- Icon Divider--> */}
+                <div className="divider-custom">
+                    <div className="divider-custom-line"></div>
+                    <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+                    <div className="divider-custom-line"></div>
+                </div>
+                {/* <!-- Contact Section Form--> */}
+                <div className="row justify-content-center">
+                    <div className="col-lg-8 col-xl-7">
+                        <a className="btn btn-outline-dark btn-social mx-1" href="//linkedin.com/in/thobanjalo-shangase"><i className="fab fa-fw fa-linkedin-in"></i></a>
+                        <a className="btn btn-outline-dark btn-social mx-1" href="//github.com/thobashangase"><i className="fab fa-fw fa-github"></i></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {/* <!-- Copyright Section--> */}
+        <div className="copyright py-4 text-center text-white">
+            <div className="container"><small>Copyright &copy; Thoba Shangase 2022</small></div>
+        </div>
+        {/* <!-- Portfolio Modals--> */}
+        {/* <!-- Portfolio Modal 1--> */}
+        <div className="portfolio-modal modal fade" id="portfolioModal1" tabIndex="-1" aria-labelledby="portfolioModal1" aria-hidden="true">
+            <div className="modal-dialog modal-xl">
+                <div className="modal-content">
+                    <div className="modal-header border-0"><button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+                    <div className="modal-body text-center pb-5">
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-8">
+                                    {/* <!-- Portfolio Modal - Title--> */}
+                                    <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Log Cabin</h2>
+                                    {/* <!-- Icon Divider--> */}
+                                    <div className="divider-custom">
+                                        <div className="divider-custom-line"></div>
+                                        <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+                                        <div className="divider-custom-line"></div>
+                                    </div>
+                                    {/* <!-- Portfolio Modal - Image--> */}
+                                    <img className="img-fluid rounded mb-5" src="assets/img/portfolio/cabin.png" alt="..." />
+                                    {/* <!-- Portfolio Modal - Text--> */}
+                                    <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                                    <button className="btn btn-primary" data-bs-dismiss="modal">
+                                        <i className="fas fa-xmark fa-fw"></i>
+                                        Close Window
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
